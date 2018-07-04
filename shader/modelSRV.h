6263ad4a14a6da60925549abc0b099d0638f0621
sugahara <a>
2018-07-05 07:17:18 +0900

    //

---
+++ shader/modelSRV.h
@@ -0,0 +1,11 @@
+/*!
+ * @brief	ƒ‚ƒfƒ‹‚ÌSRVB
+ */
+
+Texture2D<float4> lightTexture : register(t0);
+Texture2D<float4> darkTexture : register(t1);
+Texture2D<float4> depthTexture : register(t2);
+
+sampler Sampler : register(s0);
+
+
